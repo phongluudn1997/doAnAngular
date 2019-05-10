c0eac73e77f4feb4f0109c055b00bc82af385c5b
Luu Dep Trai <me@luudeptrai.com>
2019-05-10 16:09:38 +0700

    a

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/', (req, res) => res.send('Hello World'))
+
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
-console.log('Oke')
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
